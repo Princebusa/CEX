172e221da4fc96112cb624c12482720eb7af6f03
princebusa <busaprince13@gmail.com>
2026-07-26 16:20:00 +0530

    config fix in frontend

--- apps/client/src/lib/config.ts
+++ apps/client/src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000";
-export const WS_URL = "ws://localhost:3000";
+export const API_URL = process.env.BACKEND_URL;
+export const WS_URL = process.env.WS_URL;
